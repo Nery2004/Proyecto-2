4a70fd951aeb15395fee6873e5eb0f0a1c2b87ea
Albu231311 <alb231311@uvg.edu.gt>
2025-04-11 23:10:56 -0600

    int was removed in the id column, since SERIAL is int type

--- ddl.sql
+++ ddl.sql
@@ -1,30 +1,30 @@
 CREATE TABLE Usuarios (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   nombre varchar(50) NOT NULL,
   correo varchar(50) NOT NULL,
   numero varchar(50) NOT NULL
 );
 
 CREATE TABLE Estados (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   estado varchar(50) NOT NULL
 );
 
 CREATE TABLE Lugares (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   lugar varchar(50) NOT NULL,
   direccion varchar(100) NOT NULL,
   capacidad int NOT NULL
 );
 
 CREATE TABLE Categorias (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   categoria varchar(50) NOT NULL,
   precio decimal(10,2) NOT NULL
 );
 
 CREATE TABLE Eventos (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   lugar_id int NOT NULL,
   evento varchar(50) NOT NULL,
   descripcion text NOT NULL,
@@ -32,14 +32,14 @@ CREATE TABLE Eventos (
 );
 
 CREATE TABLE Asientos (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   lugar_id int NOT NULL,
   categoria_id int NOT NULL,
   ubicacion varchar(100)
 );
 
 CREATE TABLE Reservas (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   usuario_id int NOT NULL,
   evento_id int NOT NULL,
   estado_id int NOT NULL,
@@ -49,7 +49,7 @@ CREATE TABLE Reservas (
 );
 
 CREATE TABLE ReservaDetalle (
-  id int SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   reserva_id int NOT NULL,
   asiento_id int NOT NULL
 );
